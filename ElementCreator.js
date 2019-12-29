0febad384d8e38558503614d2b4506890634c31a
Kento Chiba <c.kento.chiba@gmail.com>
2019-12-29 18:24:56 +0900

    update

--- ElementCreator.js
+++ ElementCreator.js
@@ -1,13 +1,13 @@
 class ElementCreator {
-    createElement = ( parent, {type, options, children = [] }) => {
+    createElement = (parent, { type, options = {}, children = [] }) => {
         const element = document.createElement(type)
-        for ( const [key,value] of Object.entries(options) ) {
+        for (const [key, value] of Object.entries(options)) {
             element[key] = value
         }
         parent.appendChild(element)
 
         children.forEach((child) => {
-            this.createElement( element, child )
+            this.createElement(element, child)
         })
     }
 }
